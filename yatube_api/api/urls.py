94cc55e718c4c09b32340f885666fe0259975b3b
Kseniia_O <liprekqn@gmail.com>
2023-06-18 01:58:58 +0300

    Version 1.3 before review

--- yatube_api/api/urls.py
+++ yatube_api/api/urls.py
@@ -9,13 +9,13 @@ app_name = 'api'
 router_v1 = routers.DefaultRouter()
 
 router_v1.register(
-    'posts', PostViewSet, basename='posts')
+    r'posts', PostViewSet, basename='posts')
 router_v1.register(
-    'groups', GroupViewSet, basename='groups')
+    r'groups', GroupViewSet, basename='groups')
 router_v1.register(
     r'^posts/(?P<post_id>\d+)/comments', CommentViewSet, basename='comments')
 router_v1.register(
-    'follow', FollowViewSet, basename='follow')
+    r'follow', FollowViewSet, basename='follow')
 
 urlpatterns = [
     path('v1/', include('djoser.urls.jwt'), name='jwt'),
