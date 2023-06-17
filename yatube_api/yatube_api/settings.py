a460861b65d83ef9e2555157d7526baf50ae0c3f
Kseniia_O <liprekqn@gmail.com>
2023-06-17 22:27:15 +0300

    War with PEP. Round 1

--- yatube_api/yatube_api/settings.py
+++ yatube_api/yatube_api/settings.py
@@ -2,9 +2,15 @@ from pathlib import Path
 
 from datetime import timedelta
 
+import os
+
+from dotenv import load_dotenv
+
+
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-SECRET_KEY = 'hhz7l-ltdismtf@bzyz+rple7*s*w$jak%whj@(@u0eok^f9k4'
+load_dotenv()
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 DEBUG = True
 
